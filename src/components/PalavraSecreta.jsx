b44f5376df3b0c32c7fe8a16b2f6f2c41c3308e6
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-02-26 17:26:02 -0300

    Mesma palavra

--- src/components/PalavraSecreta.jsx
+++ src/components/PalavraSecreta.jsx
@@ -1,11 +1,13 @@
 import palavras from "../palavras";
+let pRandom = "";
 
 function PalavraSecreta(){
-    let pRandom = palavras[Math.floor(Math.random() * palavras.length)];
-    let DistrinchaPRandom = pRandom.split('');
-    let letraPorLetra = DistrinchaPRandom.map((d, i) => <p key={i} className="pLetraLetraPorLetra">{d}</p> )
-    let letraPorLetraSecreta = DistrinchaPRandom.map((d, i) => <p key={i} className="pLetraLetraPorLetra">{"_"}</p> )
-    console.log(DistrinchaPRandom)
+    if(pRandom === "")
+        pRandom = palavras[Math.floor(Math.random() * palavras.length)];
+        let DistrinchaPRandom = pRandom.split('');
+        let letraPorLetra = DistrinchaPRandom.map((d, i) => <p key={i} className="pLetraLetraPorLetra">{d}</p> )
+        let letraPorLetraSecreta = DistrinchaPRandom.map((d, i) => <p key={i} className="pLetraLetraPorLetra">{"_"}</p> )
+        console.log(DistrinchaPRandom)
     return(
         <div className="palavraSecretaLetraPorLetra">{letraPorLetraSecreta}</div>
         
