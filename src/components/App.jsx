6d8d6445eed5926cf0a53c48fadfc9b5c30152c3
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-02-27 02:51:15 -0300

    Botões funcionais

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import Jogo from "./Jogo";
 import Letras from "./Letras";
 
 function App() {
-  const [desativa, setDesativa] = React.useState(true)
+  const [desativa, setDesativa] = React.useState(true);
+  const [usada, setUsada] = React.useState([]);
+  const [alfabetoState, setAlfabetoState] = React.useState([]);
   return (
     <div className="App">
       <Jogo desativa={desativa} setDesativa={setDesativa} />
-      <Letras desativa={desativa} setDesativa={setDesativa}/>
+      <Letras desativa={desativa} usada={usada} setUsada={setUsada} alfabetoState={alfabetoState} setAlfabetoState={setAlfabetoState}/>
     </div>
   );
 }
--- src/components/Letras.jsx
+++ src/components/Letras.jsx
@@ -4,11 +4,16 @@ import React, { useState } from 'react';
 
 const alfabeto = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-function Letras({desativa, setDesativa}){
-    let letras = alfabeto.map((a, i) => <button disabled={desativa} key={i} className="letra"><p className="pLetra">{a.toUpperCase()}</p></button>)
+function Letras({desativa, usada, setUsada, setAlfabetoState, alfabetoState}){
+    setAlfabetoState(alfabeto);
+    function letraClicada(a){
+        const novo = [...usada, a];
+        setUsada(novo);
+        console.log(novo);
+    }
     return(
         <div className="letraContainer">
-               {letras}
+            {alfabetoState.map((a, i) => <button onClick={() => letraClicada(a)} disabled={desativa} key={i} className="letra"><p className="pLetra">{a.toUpperCase()}</p></button>)}        
         </div>
     )
 }
